d73ab3f56d64e8c4caff3b2a86496390cabd2e59
Colin Vande Vijvere <60828613+colinVDV@users.noreply.github.com>
2022-03-01 15:01:17 -0600

    Tutorial 2
    finished

--- app.js
+++ app.js
@@ -2,23 +2,37 @@ const app = Vue.createApp({
     data() {
         return {
             showBooks: true,
-            title: 'The final empire',
-            author: 'JK Rowling',
-            age: 33
+            x: 0,
+            y:0,
+            url: 'https://www.google.com/',
+            books: [
+                {title: 'book 1', author: 'random dude', age: 1000, img: 'assets/book1.jpg', isFav: true},
+                {title: 'book 2', author: 'random dudette', age: 100, img: 'assets/book2.jpg', isFav: false},
+                {title: 'book 3', author: 'random animal', age: 1300, img: 'assets/book3.jpg', isFav: true}
+            ]
         }
     },
 
     methods: {
-        decreaseAge() {
-            this.age -=1
-        },
-
-        changeTitle(title) {
-            this.title = title
-        }
-        ,
         toggleShowBooks() {
             this.showBooks = !this.showBooks
+        },
+        handleEvent(e){
+            console.log(e, e.type)
+        },
+        handleMouseMove(e) {
+            this.x = e.offsetX
+            this.y = e.offsetY
+        },
+        toggleFav(e, bookIn) {
+            bookIn.isFav = !bookIn.isFav
+            console.log(e, e.type)
+        }
+    },
+
+    computed: {
+        filteredBooks() {
+            return this.books.filter((book) => book.isFav)
         }
     }
 
